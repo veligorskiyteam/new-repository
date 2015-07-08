ef2d42915295803a310393a1acb4d83ed5700aff
fanpul <denis.leonov1990@gmail.com>
2015-07-08 18:35:24 +0300

    add tech

---
+++ js/scripts.js
@@ -0,0 +1,20 @@
+$(function(){
+    var $buttons = $('.left-menu');
+    $buttons.on('click', function(e){
+        e.preventDefault();
+        $buttons.removeClass('active');
+        $(this).addClass('active');
+    });
+
+    $('#1b').on('click', function(){
+        //$('.subMenu1').slideToggle(600);
+        $('.products').removeClass('vis-content');
+        $('.pults').addClass('vis-content');
+    });
+
+    $('#2b').on('click', function(){
+        //$('.subMenu2').slideToggle(600);
+        $('.products').removeClass('vis-content');
+        $('.tech').addClass('vis-content');
+    });                         
+});
